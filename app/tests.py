dbf17d64d21907d3e472e660d6fd1b37603e8beb
xavier <xavierperezjimenez4@gmail.com>
2025-10-05 09:07:04 -0500

    1.1.0

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import ExampleModel
 
-# Create your tests here.
+# test de modelos
+class NombreTestCase(TestCase):
+    def setUp(self):
+        Nombre = ExampleModel.objects.create(name="xavier")
+        return super().setUp()
+
+    def test_names(self):
+        xavier = ExampleModel.objects.get(name="xavier")
+        self.assertEqual(xavier.name, "xavier")
+
+# test de vista
